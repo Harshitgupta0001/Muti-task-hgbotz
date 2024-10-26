a0b0114d9d5c787c3fa944bc0d5522897766917d
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-10-26 15:51:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,7 @@ import os
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002166149059').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
