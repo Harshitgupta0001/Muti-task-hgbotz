b5f3d73cb45e39ee323fc1de17ce8fd6f81c56bb
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-10-26 15:46:47 +0530

    Update password.py

--- plugins/password.py
+++ plugins/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Updates', url='https://t.me/Silicon_Bot_Update')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Updates', url='https://t.me/hgbotz')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
